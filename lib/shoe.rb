a41bbb7c9b9609ff99bdce7f0c0c74bd9858f145
Adam G Horn <hornadam8@gmail.com>
2020-01-29 15:01:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,7 @@ class Shoe
   end
   
   def cobble
-    puts "The shoe has been repaired"
-    @shoe.condition = "new"
+    puts "Your shoe is as good as new!"
+    @shoe.condition("new")
   end
 end
